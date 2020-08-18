cbd1793dd390addb787fea3e1e05812b03e841ab
Micah Neitz <micalobia2@gmail.com>
2020-08-18 13:18:32 -0700

    Fixed the raycasting
    Tack on the CustomLightSource (as well as a Mesh Filter) onto an object to make it a light. Use MeshRender for debuging to see the light

--- Project TalkingTrees/Assets/Assets/Scripts/CustomLightSource.cs
+++ Project TalkingTrees/Assets/Assets/Scripts/CustomLightSource.cs
@@ -4,48 +4,72 @@ using UnityEngine;
 
 public class CustomLightSource : MonoBehaviour
 {
-    [SerializeField]
-    public int RayCount = 90;
-    [SerializeField]
-    public float FOV = 90f;
-    [SerializeField]
-    public float ViewDistance = 3;
+    [SerializeField] public float FOV = 90f;
+    [SerializeField] public float ViewDistance = 3f;
+    [SerializeField] public float Angle = 0f;
+    [SerializeField] public LayerMask LayerMask;
+
+    private Vector3 origin = Vector3.zero;
+    private int rayCount = 90;
+    private float startingAngle => Angle - FOV / 2f;
+
+    private Mesh mesh;
+    public Mesh Mesh => mesh;
 
     private static Vector3 GetVectorFromAngle(float angle)
     {
         float angleRad = angle * (Mathf.PI / 180f);
         return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
     }
+    private static float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir.Normalize();
+        float n = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+        return n;
+    }
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
 
     void Update()
     {
-        Mesh mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-        Vector3 origin = Vector3.zero;
-        Vector3[] vertices = new Vector3[RayCount + 2];
-        Vector2[] uv = new Vector2[vertices.Length];
-        int[] triangles = new int[RayCount * 3];
+        SetOrigin(transform.position);
+    }
 
-        vertices[0] = origin;
-        float angle = 0f;
+    void LateUpdate()
+    {
+        GenerateMesh();
+    }
+
+    void SetOrigin(Vector3 origin) => this.origin = origin;
+
+    Mesh GenerateMesh()
+    {
+        Vector3[] vertices = new Vector3[rayCount + 2];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = Vector3.zero;
+        float angle = -startingAngle;
         int vertexIndex = 1, triangleIndex = 0;
-        float angleChange = FOV / RayCount;
-        for (int i = 0; i <= RayCount; i++)
+        float angleChange = FOV / rayCount;
+        int mask = LayerMask.GetMask("Level");
+        for (int i = 0; i <= rayCount; i++)
         {
-            Vector3 vecAng = /*(Input.mousePosition - origin).normalized;*/GetVectorFromAngle(angle);
+            Vector3 vecAng = GetVectorFromAngle(angle);
             Vector3 vertex;
-            RaycastHit2D rayHit = Physics2D.Raycast(origin, vecAng, ViewDistance);
-            if(rayHit.collider == null)
+            RaycastHit2D rayHit = Physics2D.Raycast(origin, vecAng, ViewDistance, LayerMask); //Should only hit colliders with the Level tag, doesn't hit anything though
+            vertex = (rayHit.collider == null) ? origin + vecAng * ViewDistance : (Vector3)rayHit.point; //Get where we hit if we hit, otherwise just put the point at our view distance
+            //Debug.DrawRay(origin, vertex);
+            //if (rayHit.collider != null) Debug.Log("Hit somethin'");
+            vertices[vertexIndex] = vertex - transform.position;
+            if (i != 0)
             {
-                vertex = origin + vecAng * ViewDistance;
-            } else
-            {
-                vertex = rayHit.point;
-            }
-            vertices[vertexIndex] = vertex;
-            if (i > 0)
-            {
-                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex] = 0;
                 triangles[triangleIndex + 1] = vertexIndex - 1;
                 triangles[triangleIndex + 2] = vertexIndex;
                 triangleIndex += 3;
@@ -53,26 +77,9 @@ public class CustomLightSource : MonoBehaviour
             vertexIndex++;
             angle -= angleChange;
         }
-
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
-
+        return mesh;
     }
-    //[SerializeField]
-    //private float _width; //Angle in degrees
-    //[SerializeField]
-    //private float _strength; //Strength of light
-
-
-    //public float Width
-    //{
-    //    get => _width;
-    //    set => _width = value < 360f ? value : 360f;
-    //}
-    //public float Strength
-    //{
-    //    get => _strength;
-    //    set => _strength = value;
-    //}
 }
--- Project TalkingTrees/Assets/Assets/Scripts/LightOverlay.cs
+++ Project TalkingTrees/Assets/Assets/Scripts/LightOverlay.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class LightOverlay : MonoBehaviour
 {
+    /*
+     * The plan for this class is to use CustomLightSource.Mesh as a mask
+     * That way, a sprite object will have two sprites
+     * One that's lit up, and the other that's dark
+     * It'll prolly check all CustomLightSources in the scene, might have to have it check by distance for performance
+     */
+
     // Start is called before the first frame update
     void Start()
     {
